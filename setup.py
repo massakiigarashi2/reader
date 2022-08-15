36743eb7a6a1792a1c353cc61c57f172fef94c40
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2022-08-15 16:42:51 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r") as fh:
+	description = fh.read()
+
+setuptools.setup(
+	name="test-package",
+	version="0.0.1",
+	author="Massaki",
+	author_email="prof.massaki@gmail.com",
+	packages=["test_package"],
+	description="A sample test package",
+	long_description=description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/massakiigarashi2/reader",
+	license='MIT',
+	python_requires='>=3.8',
+	install_requires=[]
+)
